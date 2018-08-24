b9075d45b66ec27e2d6cf309329dc5cb7aa7c0e1
sugarshin <shinsugar@gmail.com>
2018-08-24 20:13:48 +0900

    Unique

--- model.js
+++ model.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose')
 
-const stockSchema = new mongoose.Schema({
+const StockSchema = new mongoose.Schema({
   userId: { type: String, trim: true, required: true },
   restaurantId: { type: String, trim: true, required: true },
   createdAt: { type: Date, default: Date.now, index: true },
   updatedAt: { type: Date, default: Date.now },
 })
 
-module.exports = mongoose.model('Stock', stockSchema)
+StockSchema.index({ userId: 1, restaurantId: 1 }, { unique: true })
+
+module.exports = mongoose.model('Stock', StockSchema)
